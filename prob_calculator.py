26c7cb9c68cf003eb8a265e1f728f35125c407b0
Gerard Hynes <gkhynes@gmail.com>
2021-05-17 22:28:27 +0100

    Fix draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,17 +3,20 @@ import random
 
 class Hat:
     def __init__(self, **kwargs):
-        balls = []
+        self.contents = []
         for key, value in kwargs.items():
-            balls.extend([key for i in range(value)])
-        self.contents = balls
+            self.contents.extend([key for i in range(value)])
 
     def draw(self, num):
         if num > len(self.contents):
             return self.contents
         else:
-            random.shuffle(self.contents)
-            return self.contents[-num:]
+            drawn = []
+            for i in range(num):
+                selected = random.choice(self.contents)
+                self.contents.remove(selected)
+                drawn.append(selected)
+            return drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
