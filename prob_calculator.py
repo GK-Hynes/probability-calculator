3da2fb84b574dca5aac53414b0b4aed1a2501765
Gerard Hynes <gkhynes@gmail.com>
2021-05-17 21:54:55 +0100

    Build out Hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,20 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        balls = []
+        for key, value in kwargs.items():
+            balls.extend([key for i in range(value)])
+        self.contents = balls
 
+    def draw(self, num):
+        if num > len(self.contents):
+            return self.contents
+        else:
+            random.shuffle(self.contents)
+            return self.contents[-num:]
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
+
